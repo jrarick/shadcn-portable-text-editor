941c0a41001a479b92fbb18a7f338bf7fd54e47b
Joshua Rarick <rarick.joshua.e@gmail.com>
2025-08-16 20:02:22 -0500

    added github link

--- app/page.tsx
+++ app/page.tsx
@@ -3,27 +3,39 @@ import { OpenInV0Button } from "@/components/open-in-v0-button"
 import CompactEditor from "@/registry/default/blocks/compact-editor"
 import ExtendedEditor from "@/registry/default/blocks/extended-editor"
 import SimpleEditor from "@/registry/default/blocks/simple-editor"
+import { buttonVariants } from "@/registry/default/components/ui/button"
+import { GithubIcon } from "lucide-react"
+
 // This page displays items from the custom registry.
 // You are free to implement this with your own design as needed.
 
 export default function Home() {
   return (
     <div className="mx-auto flex min-h-svh max-w-5xl flex-col gap-8 px-4 py-8">
-      <header className="flex flex-col gap-1">
-        <h1 className="text-3xl font-bold tracking-tight">
-          Portable Text Editor
-        </h1>
-        <p className="text-muted-foreground">
-          Portable text editor examples installable via the{" "}
-          <a
-            href="https://ui.shadcn.com/"
-            className="font-semibold text-foreground underline"
-          >
-            shadcn
-          </a>{" "}
-          registry.
-        </p>
-      </header>
+      <div className="flex justify-between">
+        <header className="flex flex-col gap-1">
+          <h1 className="text-3xl font-bold tracking-tight">
+            Portable Text Editor
+          </h1>
+          <p className="text-muted-foreground">
+            Portable text editor examples installable via the{" "}
+            <a
+              href="https://ui.shadcn.com/"
+              className="font-semibold text-foreground underline"
+            >
+              shadcn
+            </a>{" "}
+            registry.
+          </p>
+        </header>
+        <a
+          href="https://github.com/jrarick/shadcn-portable-text-editor"
+          target="_blank"
+          className={buttonVariants({ variant: "default", size: "icon" })}
+        >
+          <GithubIcon />
+        </a>
+      </div>
       <main className="flex flex-1 flex-col gap-8">
         <div className="relative flex flex-col gap-4 rounded-lg border p-4">
           <div className="flex items-center justify-between">
